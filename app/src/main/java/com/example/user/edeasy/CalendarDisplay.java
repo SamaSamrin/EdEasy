e386e4f55056a434b2e36f71d4c5f990eab31764
Sama Samrin <samasamrin@gmail.com>
2017-05-27 21:06:10 +0600

    removing caldroid temporarily, added scrollView to tab contents

--- app/src/main/java/com/example/user/edeasy/CalendarDisplay.java
+++ app/src/main/java/com/example/user/edeasy/CalendarDisplay.java
@@ -1,14 +1,24 @@
 package com.example.user.edeasy;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
-public class CalendarDisplay extends Activity {
+//import com.roomorama.caldroid.CaldroidFragment;
+//import com.roomorama.caldroid.CaldroidListener;
+
+@SuppressLint("SimpleDateFormat")
+public class CalendarDisplay extends AppCompatActivity {
 
     ListView eventsView;
+
+//    private boolean undo = false;
+//    private CaldroidFragment caldroidFragment;
+//    private CaldroidFragment dialogCaldroidFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
