82494769cf0a6a1e694948ae392cc5cc899b3603
Sama Samrin <samasamrin@gmail.com>
2017-06-01 13:23:28 +0600

    added current routine view

--- app/src/main/java/com/example/user/edeasy/CurrentRoutine.java
+++ app/src/main/java/com/example/user/edeasy/CurrentRoutine.java
@@ -2,6 +2,7 @@ package com.example.user.edeasy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CurrentRoutine extends Activity {
 
@@ -9,5 +10,9 @@ public class CurrentRoutine extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_routine);
+    }
+
+    public void viewFullRoutine(View view){
+
     }
 }
