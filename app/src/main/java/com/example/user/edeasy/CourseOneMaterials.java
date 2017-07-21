fe3532c27cc2c3138c1631869821407024372b4a
Sama Samrin <samasamrin@gmail.com>
2017-07-21 22:07:48 +0600

    can display storage files, but UI needs fixing

--- app/src/main/java/com/example/user/edeasy/CourseOneMaterials.java
+++ app/src/main/java/com/example/user/edeasy/CourseOneMaterials.java
@@ -14,7 +14,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.example.user.edeasy.R;
@@ -50,12 +52,12 @@ public class CourseOneMaterials extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String TAG = "Course 1 Fragment";
-    final int RC_FILE_PICKER = 101;
-    ListView courseMaterialsView;
+    final int RC_UPLOAD_FILES = 101;
+    final int RC_DOWNLOAD_FILES = 102;
 
     FirebaseAuth auth;
     DatabaseReference databaseReference;
-    DatabaseReference currentUserRef;
+    DatabaseReference courseFileDownloadUrls;
     StorageReference storageReference;
     StorageReference courseOneFilesRef;
     FirebaseUser currentUser;
@@ -64,8 +66,13 @@ public class CourseOneMaterials extends Fragment {
     String courseName;
     String username;
     String sectionNumber;
-//    int numberOfCourses = 0;
-//    String[][] assignedCourses;
+    int numberOfFiles;
+    String[][] files;
+    String[] fileNames;
+    String[] fileDownloadUrls;
+    ListView courseMaterialsView;
+    String lastpath; //the last part of the file you're going to upload
+    ArrayAdapter<String> adapter;
 
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -108,6 +115,10 @@ public class CourseOneMaterials extends Fragment {
         Log.e(TAG, "onCreate");
         super.onCreate(savedInstanceState);
 
+        fileNames = new String[1];
+        fileNames[0] = "Demo";
+        fileDownloadUrls = new String[1];
+
         Bundle args = getArguments();
         if (args!=null) {
             courseName = args.getString("course");
@@ -122,29 +133,36 @@ public class CourseOneMaterials extends Fragment {
         auth = FirebaseAuth.getInstance();
         currentUser = auth.getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        courseFileDownloadUrls = databaseReference.child("departments/CSE/courses").child(courseName).child("sections").child(sectionNumber).child("file_urls");
+        Log.e(TAG, "#127: file download urls referece = "+courseFileDownloadUrls.toString());
         storageReference = FirebaseStorage.getInstance().getReference();
         courseOneFilesRef = storageReference.child("CSE").child(courseName).child(sectionNumber);
         Log.e(TAG, "#111: course reference = "+courseOneFilesRef.toString());
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.e(TAG, "onCreateView");
-        // Inflate the layout for this fragment
-        courseMaterialsView = (ListView) container.findViewById(R.id.course1_materials_listview);
         View v = inflater.inflate(R.layout.fragment_course_one_materials, container, false);
+
+        //displaying the files in ListView
+        courseMaterialsView = (ListView) v.findViewById(R.id.course1_materials_listview);
+        if (courseMaterialsView==null)
+            Log.e(TAG, "course listview is null");
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, fileNames);
+        courseMaterialsView.setAdapter(adapter);
+        displayFiles();
+
+        //upload button
         Button uploadOneButton = (Button)v.findViewById(R.id.course1_upload_button);
-        //set onclick listener
         uploadOneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //open the storage on phone
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("*/*");
                 intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                startActivityForResult(Intent.createChooser(intent, "Choose a file"), RC_FILE_PICKER);
+                startActivityForResult(Intent.createChooser(intent, "Choose a file"), RC_UPLOAD_FILES);
             }
         });
         return v;
@@ -152,19 +170,20 @@ public class CourseOneMaterials extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode==RC_FILE_PICKER){
+        Log.e(TAG, "onActivityResult");
+        //uploading files
+        if (requestCode==RC_UPLOAD_FILES){
             if (resultCode == RESULT_OK){
                 Log.e(TAG, "file received - "+data.getData().getLastPathSegment());
                 Uri uri = data.getData();
-                //Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
-                String lastpath = uri.getLastPathSegment();
+                lastpath = uri.getLastPathSegment();
                if (lastpath.contains("/")) {
                    int begin = lastpath.indexOf('/');
                    int length = lastpath.length();
                    Log.e(TAG, "begin = "+String.valueOf(begin)+" length="+String.valueOf(length));
                    lastpath = lastpath.substring(begin+1, length);
                }
-               Log.e(TAG, "#151: last path segment = "+lastpath);
+               Log.e(TAG, "#174: last path segment = "+lastpath);
                 StorageReference newDocRef = courseOneFilesRef.child(lastpath);
                 UploadTask task = newDocRef.putFile(uri);
                 task.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -172,8 +191,13 @@ public class CourseOneMaterials extends Fragment {
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         Uri uri = taskSnapshot.getDownloadUrl();
                         if (uri!=null) {
-                            Log.e(TAG, "#155 : download url of doc = " + uri.toString());
-                            Log.e(TAG, "#156 : metadata of the doc = "+ taskSnapshot.getMetadata());
+                            Log.e(TAG, "#196");
+                            lastpath = lastpath.substring(0, lastpath.indexOf('.'));
+                            //Log.e(TAG, "#182 : download url of doc = " + uri.toString());
+                            DatabaseReference newRef = courseFileDownloadUrls.child(lastpath);
+                            //Log.e(TAG, "#184 : new ref in db = "+newRef.toString());
+                            newRef.setValue(uri.toString());
+                            //Log.e(TAG, "#156 : metadata url of the doc = "+ taskSnapshot.getMetadata());
                         }
                     }
                 });
@@ -188,6 +212,43 @@ public class CourseOneMaterials extends Fragment {
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    void displayFiles(){
+        Log.e(TAG, "displayFiles");
+        courseFileDownloadUrls.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e(TAG, "onDataChange");
+                long filesNumber = dataSnapshot.getChildrenCount();
+                numberOfFiles = (int) filesNumber;
+                Log.e(TAG, "#208: files number = "+numberOfFiles);
+                fileNames = new String[numberOfFiles];
+                fileDownloadUrls = new String[numberOfFiles];
+                int i = 0;
+                for (DataSnapshot snap : dataSnapshot.getChildren()){
+                    String key = snap.getKey();
+                    Log.e(TAG, "#214 : key="+key);
+                    fileNames[i] = key;
+                    String downloadUrl = snap.getValue(String.class);
+                    fileDownloadUrls[i] = downloadUrl;
+                    i++;
+                }
+                updateListView(fileNames);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled");
+            }
+        });
+    }
+
+    void updateListView(String[] fileNames){
+        Log.e(TAG, "#247 : filenames 1 = "+fileNames[0] );
+        Log.e(TAG, "#247 : filenames 2 = "+fileNames[1] );
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, fileNames);
+        courseMaterialsView.setAdapter(adapter);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
