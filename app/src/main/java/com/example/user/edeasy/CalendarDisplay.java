8ac89a44db82d7e6faccc0151e3427c569d19fdc
Sama Samrin <samasamrin@gmail.com>
2017-07-25 18:30:11 +0600

    displaying events on Calendar

--- app/src/main/java/com/example/user/edeasy/CalendarDisplay.java
+++ app/src/main/java/com/example/user/edeasy/CalendarDisplay.java
@@ -2,13 +2,19 @@ package com.example.user.edeasy;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -35,6 +41,8 @@ public class CalendarDisplay extends AppCompatActivity {
     String[] departments;
     String course;
     String section;
+    ListAdapter calendarAdapter;
+    int index = 1;
 
     FirebaseAuth auth;
     DatabaseReference databaseReference;
@@ -84,6 +92,7 @@ public class CalendarDisplay extends AppCompatActivity {
 
         events = new String[numberOfCourses][2];
         eventsView = (ListView) findViewById(R.id.calendar_events_display);
+       // calendarAdapter = new CalendarEventsAdapter(this, events);
         //ListAdapter adapter = new ArrayAdapter<String>();
 
         getEventsFromDatabase();
@@ -97,26 +106,93 @@ public class CalendarDisplay extends AppCompatActivity {
                     .child("events");
             Log.e(TAG, "#92 : events ref = "+eventsRef.toString());
             //departments/CSE/courses/CSE110/sections/1/events
-            eventsRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    long eventsCount = dataSnapshot.getChildrenCount();
+        }
+        allDepartmentsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (int i=0; i<numberOfCourses; i++) {
+                    DataSnapshot ds = dataSnapshot.child(departments[i])
+                            .child("courses").child(assignedCourses[i][0])
+                            .child("sections").child(assignedCourses[i][1])
+                            .child("events");
+                    long eventsCount = ds.getChildrenCount();
                     int numberOfEvents = (int) eventsCount;
-                    Log.e(TAG, "#104 : "+String.valueOf(numberOfEvents));
-                    int i = 1;
-                    for (DataSnapshot snap : dataSnapshot.getChildren() ){
+                    Log.e(TAG, "#104 : " + String.valueOf(numberOfEvents));
+                    for (DataSnapshot snap : ds.getChildren()) {
+                        //Log.e(TAG, "index=" + String.valueOf(i));
                         events[i][0] = snap.child("name").getValue(String.class);
                         Log.e(TAG, "#106 : event name = " + events[i][0]);
                         events[i][1] = snap.child("due date").getValue(String.class);
-                        Log.e(TAG, "#110 : due date = "+events[i][1]);
+                        Log.e(TAG, "#110 : due date = " + events[i][1]);
                     }
-                }
+                    calendarAdapter = new CalendarEventsAdapter(CalendarDisplay.this, events);
+                    eventsView.setAdapter(calendarAdapter);
+                    //index++;
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
                 }
-            });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+}
+
+class CalendarEventsAdapter extends BaseAdapter{
+
+    private static final String TAG = "**Calendar Adapter**";
+
+    int numberOfEvents;
+    String[] eventNames;
+    String[] eventDates;
+    Context context;
+    private static LayoutInflater inflater = null;
+
+    CalendarEventsAdapter(Context c, String[][] eventsInput){
+        numberOfEvents = eventsInput.length;
+        eventNames = new String[numberOfEvents];
+        eventDates = new String[numberOfEvents];
+        context = c;
+        for (int i = 0; i < numberOfEvents; i++) {
+            eventNames[i] = eventsInput[i][0];
+            eventDates[i] = eventsInput[i][1];
+            Log.e(TAG, "at i="+String.valueOf(i)+"event name = "+eventNames[i]+" on "+eventDates[i]);
         }
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfEvents;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return eventDates[position]+eventNames[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.e(TAG, "position="+position);
+        View view = convertView;
+        if (view == null ) {
+            view = inflater.inflate(R.layout.calendar_event_item, null);
+        }
+        TextView event_tv = (TextView) view.findViewById(R.id.event_textview);
+        TextView date_tv = (TextView) view.findViewById(R.id.date_textview);
+        if (eventNames[position]!=null && eventDates[position]!=null){
+            event_tv.setText(eventNames[position]);
+            date_tv.setText(eventDates[position]);
+        }
+        return view;
     }
 }
