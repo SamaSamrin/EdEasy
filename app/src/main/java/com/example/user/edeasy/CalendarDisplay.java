20499326dfd7cd3fc90fcb2ad684ee327b8b63e8
Sama Samrin <samasamrin@gmail.com>
2017-07-26 11:33:18 +0600

    month-wise events display worked partially

--- app/src/main/java/com/example/user/edeasy/CalendarDisplay.java
+++ app/src/main/java/com/example/user/edeasy/CalendarDisplay.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.CalendarView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -25,6 +27,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.Calendar;
 
 //import com.roomorama.caldroid.CaldroidFragment;
 //import com.roomorama.caldroid.CaldroidListener;
@@ -43,6 +47,7 @@ public class CalendarDisplay extends AppCompatActivity {
     String section;
     ListAdapter calendarAdapter;
     int index = 1;
+    CalendarView calendarView;
 
     FirebaseAuth auth;
     DatabaseReference databaseReference;
@@ -92,6 +97,7 @@ public class CalendarDisplay extends AppCompatActivity {
 
         events = new String[numberOfCourses][2];
         eventsView = (ListView) findViewById(R.id.calendar_events_display);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
        // calendarAdapter = new CalendarEventsAdapter(this, events);
         //ListAdapter adapter = new ArrayAdapter<String>();
 
@@ -125,7 +131,8 @@ public class CalendarDisplay extends AppCompatActivity {
                         events[i][1] = snap.child("due date").getValue(String.class);
                         Log.e(TAG, "#110 : due date = " + events[i][1]);
                     }
-                    calendarAdapter = new CalendarEventsAdapter(CalendarDisplay.this, events);
+                    calendarAdapter = new CalendarEventsAdapter(CalendarDisplay.this,
+                            events, calendarView);
                     eventsView.setAdapter(calendarAdapter);
                     //index++;
 
@@ -151,8 +158,14 @@ class CalendarEventsAdapter extends BaseAdapter{
     String[] eventDates;
     Context context;
     private static LayoutInflater inflater = null;
+    CalendarView calendar;
+    String currentMonth = convertToMonthName(Calendar.getInstance().get(Calendar.MONTH));
+    //String todaysDate = currentMonth+" "+String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+    String selectedDate;
 
-    CalendarEventsAdapter(Context c, String[][] eventsInput){
+    CalendarEventsAdapter(Context c, String[][] eventsInput, CalendarView cal){
+        //Log.e(TAG, "today's date = "+todaysDate);
+        Log.e(TAG, "month = "+currentMonth);
         numberOfEvents = eventsInput.length;
         eventNames = new String[numberOfEvents];
         eventDates = new String[numberOfEvents];
@@ -163,6 +176,129 @@ class CalendarEventsAdapter extends BaseAdapter{
             Log.e(TAG, "at i="+String.valueOf(i)+"event name = "+eventNames[i]+" on "+eventDates[i]);
         }
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        calendar = cal;
+        calendarListenerSetup();
+    }
+
+    private String dateToWords(String date){
+        String dateInWords = "";
+        //Log.e(TAG, "date="+date);
+        if (date!=null){
+        String[] sections = date.split("\\.");
+            if (sections.length>0) {
+                String day = sections[0];
+                String month = convertToMonthName(sections[1]);
+                String year = sections[2];
+                dateInWords = month + " " + day + "," + year;
+            }else {
+                Log.e(TAG, "splitted array length is zero ");
+                dateInWords = date;
+            }
+        }else{
+            Log.e(TAG, "date argument is null");
+        }
+        return dateInWords;
+    }
+
+    String convertToMonthName(String monthNumber){
+        String month = "";
+        switch (monthNumber) {
+            case "01":
+                month = "January";
+                break;
+            case "02":
+                month = "February";
+                break;
+            case "03":
+                month = "March";
+                break;
+            case "04":
+                month = "April";
+                break;
+            case "05":
+                month = "May";
+                break;
+            case "06":
+                month = "June";
+                break;
+            case "07":
+                month = "July";
+                break;
+            case "08":
+                month = "August";
+                break;
+            case "09":
+                month = "September";
+                break;
+            case "10":
+                month = "October";
+                break;
+            case "11":
+                month = "November";
+                break;
+            case "12":
+                month = "December";
+                break;
+        }
+        return  month;
+    }
+
+    String convertToMonthName(int monthNumber){
+        String month = "";
+        switch (monthNumber) {
+            case 0:
+                month = "January";
+                break;
+            case 1:
+                month = "February";
+                break;
+            case 2:
+                month = "March";
+                break;
+            case 3:
+                month = "April";
+                break;
+            case 4:
+                month = "May";
+                break;
+            case 5:
+                month = "June";
+                break;
+            case 6:
+                month = "July";
+                break;
+            case 7:
+                month = "August";
+                break;
+            case 8:
+                month = "September";
+                break;
+            case 9:
+                month = "October";
+                break;
+            case 10:
+                month = "November";
+                break;
+            case 11:
+                month = "December";
+                break;
+        }
+        return  month;
+    }
+
+    void calendarListenerSetup(){
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String dateInt = String.valueOf(dayOfMonth)+"."+
+                        String.valueOf(month+1)+"."+
+                        String.valueOf(year);
+                selectedDate = dateToWords(dateInt);
+                currentMonth = convertToMonthName(month);
+                notifyDataSetChanged();
+                Log.e(TAG, "selected month = "+currentMonth);
+            }
+        });
     }
 
     @Override
@@ -182,16 +318,32 @@ class CalendarEventsAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Log.e(TAG, "position="+position);
+        if (calendar!=null) {
+            //Log.e(TAG, "calendar date = " + calendar.getDate());
+        }else
+            Log.e(TAG, "calendar is null");
+
         View view = convertView;
         if (view == null ) {
             view = inflater.inflate(R.layout.calendar_event_item, null);
         }
         TextView event_tv = (TextView) view.findViewById(R.id.event_textview);
         TextView date_tv = (TextView) view.findViewById(R.id.date_textview);
+
         if (eventNames[position]!=null && eventDates[position]!=null){
-            event_tv.setText(eventNames[position]);
-            date_tv.setText(eventDates[position]);
+            String month = (dateToWords(eventDates[position]).split(" ")) [0];
+            Log.e(TAG, "month="+month+" current month="+currentMonth);
+            //if (month.equals(currentMonth)) {
+            // --> working but, keeping the other list items empty
+            // sort the events or keep them in monthlyEvents[][]
+            // where the first index will be the months's number or name
+            // and the second index will carry the events' names and dates
+                event_tv.setText(eventNames[position]);
+                date_tv.setText(dateToWords(eventDates[position]));
+//            }else{
+//                event_tv.setText("");
+//                date_tv.setText("");
+//            }
         }
         return view;
     }
